a6861f411da24399e4cca63eccb90a95ecf5a8eb
Vibhanshu Dubey <vibhanshudeepakdubey@gmail.com>
2025-03-26 13:28:56 +0530

    setup backend

---
+++ backend/src/test/java/com/shortify/url/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortify.url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
