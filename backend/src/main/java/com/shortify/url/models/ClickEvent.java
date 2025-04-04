696f7fdc9027751c67bc42e0f8e1ec8927dbd2dd
Vibhanshu Dubey <vibhanshudeepakdubey@gmail.com>
2025-04-04 17:21:07 +0530

    update click event model

--- backend/src/main/java/com/shortify/url/models/ClickEvent.java
+++ backend/src/main/java/com/shortify/url/models/ClickEvent.java
@@ -1,4 +1,20 @@
 package com.shortify.url.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class ClickEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime clickDate;
+
+    @ManyToOne
+    @JoinColumn(name = "url_mapping_id")
+    private UrlMapping urlMapping;
 }
