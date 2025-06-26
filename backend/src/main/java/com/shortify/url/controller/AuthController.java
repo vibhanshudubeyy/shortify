cd3c6107a425058b74cda316261fb295019a72e5
Vibhanshu Dubey <vibhanshudeepakdubey@gmail.com>
2025-06-26 14:53:05 +0530

    added controllers

---
+++ backend/src/main/java/com/shortify/url/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.shortify.url.controller;
+
+import com.shortify.url.dtos.RegisterRequest;
+import com.shortify.url.models.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+
+public class AuthController {
+
+    private UserService
+
+    @PostMapping("/public/register")
+    public ResponseEntity<?> registerUser(
+            @RequestBody RegisterRequest registerRequest){
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(registerRequest.getPassword());
+        user.setEmail(registerRequest.getEmail());
+        user.setRole("ROLE_USER");
+
+        userService.registerUser(user);
+        return ResponseEntity.ok("User registered successfully");
+    }
+}
