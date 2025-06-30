beab23a934439c009bbec7c3bb4847386958364c
Vibhanshu Dubey <vibhanshudeepakdubey@gmail.com>
2025-07-01 00:18:21 +0530

    Created UrlMappingController & added endpoint shorten

---
+++ backend/src/main/java/com/shortify/url/controller/UrlMappingController.java
@@ -0,0 +1,36 @@
+package com.shortify.url.controller;
+
+import com.shortify.url.dtos.UrlMappingDTO;
+import com.shortify.url.models.User;
+import com.shortify.url.service.UrlMappingService;
+import com.shortify.url.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/urls")
+@AllArgsConstructor
+public class UrlMappingController {
+    private UrlMappingService urlMappingService;
+    private UserService userService;
+
+    @PostMapping("/shorten")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<UrlMappingDTO> createShortUrl(
+            @RequestBody Map<String, String> request,
+            Principal principal
+    ){
+        String originalUrl = request.get("originalUrl");
+        User user = userService.findByUsername(principal.getName());
+        UrlMappingDTO urlMappingDTO = urlMappingService.createShortUrl(originalUrl, user);
+        return ResponseEntity.ok(urlMappingDTO);
+    }
+}
