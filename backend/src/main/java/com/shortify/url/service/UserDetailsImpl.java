35179ef53e21f5ca7c6cf07075027e7cdcf4560a
Vibhanshu Dubey <vibhanshudeepakdubey@gmail.com>
2025-06-30 10:37:25 +0530

    removed unnecessary imports

--- backend/src/main/java/com/shortify/url/service/UserDetailsImpl.java
+++ backend/src/main/java/com/shortify/url/service/UserDetailsImpl.java
@@ -33,8 +33,8 @@ public class UserDetailsImpl implements UserDetails {
         return new UserDetailsImpl(
                 user.getId(),
                 user.getUsername(),
-                user.getEmail(),
                 user.getPassword(),
+                user.getEmail(),
                 Collections.singletonList(authority)
         );
     }
