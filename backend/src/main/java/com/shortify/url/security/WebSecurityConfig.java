f1819002df6ba8b6ef7b8024b83d46ad052b7939
Vibhanshu Dubey <vibhanshudeepakdubey@gmail.com>
2025-06-30 10:33:58 +0530

    fixed minor bugs

--- backend/src/main/java/com/shortify/url/security/WebSecurityConfig.java
+++ backend/src/main/java/com/shortify/url/security/WebSecurityConfig.java
@@ -5,7 +5,9 @@ import com.shortify.url.service.UserDetailsServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,6 +35,7 @@ public class WebSecurityConfig {
         return new JwtAuthenticationFilter();
     }
 
+    @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService);
@@ -56,5 +59,11 @@ public class WebSecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
 
 }
