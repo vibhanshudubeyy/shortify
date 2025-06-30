dcefc0061992242685dc1e150504eec06fc47838
Vibhanshu Dubey <vibhanshudeepakdubey@gmail.com>
2025-07-01 00:17:25 +0530

    Added UrlMappingService methods

--- backend/src/main/java/com/shortify/url/service/UrlMappingService.java
+++ backend/src/main/java/com/shortify/url/service/UrlMappingService.java
@@ -1,12 +1,53 @@
 package com.shortify.url.service;
 
 import com.shortify.url.dtos.UrlMappingDTO;
+import com.shortify.url.models.UrlMapping;
+import com.shortify.url.models.User;
+import com.shortify.url.repository.UrlMappingRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Random;
+
+@AllArgsConstructor
 @Service
 public class UrlMappingService {
 
-    public UrlMappingDTO createShortUrl(String originalUrl, String shortUrl) {
+    private UrlMappingRepository urlMappingRepository;
 
+    public UrlMappingDTO createShortUrl(String originalUrl, User user) {
+        String shortUrl = generateShortUrl();
+        UrlMapping urlMapping = new UrlMapping();
+        urlMapping.setOriginalUrl(originalUrl);
+        urlMapping.setShortUrl(shortUrl);
+        urlMapping.setUser(user);
+        urlMapping.setCreatedDate(LocalDateTime.now());
+
+        UrlMapping savedUrlMapping = urlMappingRepository.save(urlMapping);
+        return convertToDto(savedUrlMapping);
+    }
+
+    private UrlMappingDTO convertToDto(UrlMapping urlMapping) {
+        UrlMappingDTO dto = new UrlMappingDTO();
+        dto.setId(urlMapping.getId());
+        dto.setOriginalUrl(urlMapping.getOriginalUrl());
+        dto.setShortUrl(urlMapping.getShortUrl());
+        dto.setClickCount(urlMapping.getClickCount());
+        dto.setCreatedAt(urlMapping.getCreatedDate());
+        dto.setUsername(urlMapping.getUser().getUsername());
+        return dto;
+    }
+
+    private String generateShortUrl() {
+
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder shortUrl = new StringBuilder(8);
+
+        for(int i=0; i<8; i++) {
+            shortUrl.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        return shortUrl.toString();
     }
 }
