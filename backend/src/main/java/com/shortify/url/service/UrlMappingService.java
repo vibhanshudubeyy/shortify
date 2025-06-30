99485b4dff1e384070f8ce80abd7aea45944a20a
Vibhanshu Dubey <vibhanshudeepakdubey@gmail.com>
2025-06-30 23:45:11 +0530

    added UrlMappingService

---
+++ backend/src/main/java/com/shortify/url/service/UrlMappingService.java
@@ -0,0 +1,12 @@
+package com.shortify.url.service;
+
+import com.shortify.url.dtos.UrlMappingDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UrlMappingService {
+
+    public UrlMappingDTO createShortUrl(String originalUrl, String shortUrl) {
+
+    }
+}
