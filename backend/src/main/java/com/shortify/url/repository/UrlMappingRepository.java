9eed456d62cd734912429076536e6a3ed228c6fe
Vibhanshu Dubey <vibhanshudeepakdubey@gmail.com>
2025-07-01 00:19:24 +0530

    Added UrlMappingRepository

---
+++ backend/src/main/java/com/shortify/url/repository/UrlMappingRepository.java
@@ -0,0 +1,14 @@
+package com.shortify.url.repository;
+
+import com.shortify.url.models.UrlMapping;
+import com.shortify.url.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+    UrlMapping findByShortUrl(String shortUrl);
+    List<UrlMapping> findByUser(User user);
+}
