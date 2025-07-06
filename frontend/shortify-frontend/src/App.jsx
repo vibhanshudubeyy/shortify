ae4a6b67f892861642faee48753b46f65638ab14
Vibhanshu Dubey <vibhanshudeepakdubey@gmail.com>
2025-07-06 21:10:23 +0530

    minor fixes

--- frontend/shortify-frontend/src/App.jsx
+++ frontend/shortify-frontend/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import AboutPage from './components/AboutPage'
+import Navbar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+            <Route path='/' element={<LandingPage/>}/>
+            <Route path='/about' element={<AboutPage/>}/>
+        </Routes>
+      <Footer/>
+       </BrowserRouter>
     </>
   )
 }
