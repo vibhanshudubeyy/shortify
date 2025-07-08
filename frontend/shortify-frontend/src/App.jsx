1568c935026802ae95b99a705a89160236ca8c42
Vibhanshu Dubey <vibhanshudeepakdubey@gmail.com>
2025-07-08 16:33:51 +0530

    Added Login Page to render

--- frontend/shortify-frontend/src/App.jsx
+++ frontend/shortify-frontend/src/App.jsx
@@ -5,6 +5,9 @@ import LandingPage from './components/LandingPage'
 import AboutPage from './components/AboutPage'
 import Navbar from './components/NavBar'
 import Footer from './components/Footer'
+import RegisterPage from './components/RegisterPage'
+import LoginPage from './components/LoginPage'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
 
@@ -12,9 +15,12 @@ function App() {
     <>
       <BrowserRouter>
       <Navbar/>
+      <Toaster position='bottom-center'/>
         <Routes>
             <Route path='/' element={<LandingPage/>}/>
             <Route path='/about' element={<AboutPage/>}/>
+            <Route path='/register' element={<RegisterPage/>}/>
+            <Route path='/login' element={<LoginPage/>}/>
         </Routes>
       <Footer/>
        </BrowserRouter>
