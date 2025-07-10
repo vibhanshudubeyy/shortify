57d0d59496a958e6691c2b2802cda5b261e1d328
Vibhanshu Dubey <vibhanshudeepakdubey@gmail.com>
2025-07-10 22:53:23 +0530

    Update Routes

--- frontend/shortify-frontend/src/App.jsx
+++ frontend/shortify-frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Footer from './components/Footer'
 import RegisterPage from './components/RegisterPage'
 import LoginPage from './components/LoginPage'
 import { Toaster } from 'react-hot-toast'
+import DashboardLayout from './components/dashboard/DashboardLayout'
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
             <Route path='/about' element={<AboutPage/>}/>
             <Route path='/register' element={<RegisterPage/>}/>
             <Route path='/login' element={<LoginPage/>}/>
+            <Route path='/dashboard' element={<DashboardLayout/>}/>
         </Routes>
       <Footer/>
        </BrowserRouter>
